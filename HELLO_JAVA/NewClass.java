e60b0c2899df015d199bfd4005ccec92d118e7a0
madhavmanohars <madhav.ms@inapp.com>
2018-08-13 17:05:57 +0530

    inheritance

---
+++ HELLO_JAVA/NewClass.java
@@ -0,0 +1,24 @@
+public class NewClass
+{
+
+  public static class superclass
+  {
+    static void print()
+    {
+    System.out.println("print in superclass");
+    }
+  }
+  public static class subclass extends superclass
+  {
+    static void print()
+    {
+      System.out.println("print in subclass");
+    }
+  }
+public static void main(String[] args) {
+  superclass A = new superclass();
+  superclass B = new superclass();
+  A.print();
+  B.print();
+}
+}
